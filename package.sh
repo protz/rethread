e6355215dce97aff2afa90e64c29d7cc4e3066b7
Jonathan Protzenko <jonathan.protzenko@gmail.com>
2014-04-30 11:23:00 +0200

    Fix filename for upload

--- package.sh
+++ package.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 BRANCH=`git branch | egrep "\\* (.*)" | cut -c 3-`
 DATE=`date +%Y%m%d%H%M`
-TARGET_FILENAME="$DATE-$BRANCH.xpi"
+TARGET_FILENAME="rethread.xpi"
 
 upload() {
-  echo "cd jonathan/files\nput rethread.xpi gcv-nightlies/$TARGET_FILENAME" | ftp xulforum@ftp.xulforum.org
+  echo "cd jonathan/files\nput rethread.xpi $TARGET_FILENAME" | ftp xulforum@ftp.xulforum.org
 }
 
 ./build.sh
